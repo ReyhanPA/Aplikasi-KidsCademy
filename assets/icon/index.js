2020916fac30d1efab07c7b31c28e3eecfdec91a
Shulhan <18221051@std.stei.itb.ac.id>
2024-05-19 16:25:17 +0700

    Finish Styling LibraryScreen next SubjectScreen

--- assets/icon/index.js
+++ assets/icon/index.js
@@ -8,5 +8,10 @@ import IconMedal from "./medal.svg";
 import IconClick from "./click.svg";
 import IconClickBlack from "./click-black.svg";
 import IconArrowDown from "./arrow-down.svg";
+import IconHurufButton from "./huruf-button.svg";
+import IconBacaButton from "./baca-button.svg";
+import IconHitungButton from "./hitung-button.svg";
+import IconLogikaButton from "./logika-button.svg";
+import IconHiburanButton from "./hiburan-button.svg";
 
-export { IconDashboard, IconLibrary, IconStore, IconProfile, IconElectrifity, IconDoubleElecttrify, IconMedal, IconClick, IconClickBlack, IconArrowDown };
+export { IconDashboard, IconLibrary, IconStore, IconProfile, IconElectrifity, IconDoubleElecttrify, IconMedal, IconClick, IconClickBlack, IconArrowDown, IconHurufButton, IconBacaButton, IconHitungButton, IconLogikaButton, IconHiburanButton };
