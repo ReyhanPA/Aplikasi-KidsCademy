0e417a779fed2d5ddb06a8d2670c8ad18896aef3
Shulhan <18221051@std.stei.itb.ac.id>
2024-05-20 14:19:16 +0700

    selesai styling page material tinggal masukin konten

--- app/(tabs)/libraryScreen/[subjectScreen]/[materialScreen]/index.js
+++ app/(tabs)/libraryScreen/[subjectScreen]/[materialScreen]/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { StyleSheet, SafeAreaView, StatusBar, View, Text } from "react-native";
 import { useRoute } from '@react-navigation/native';
+import MaterialBody from "../../../../components/MaterialBody";
+import DashboardTopBar from "../../../../components/DashboardTopBar";
 const MaterialScreen = () => {
   const route = useRoute();
   
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text className="text-xl font-semibold text-black">Detail {route.params.material}</Text>
-      </View>
+      <DashboardTopBar />
+      <MaterialBody material={route.params.material}/>
     </SafeAreaView>
   )
 };
